84b1506a5d2c442eb9eff46eab0bb3ddd2c3d0bd
StoneShiLei <569812422@qq.com>
2022-04-13 22:29:05 +0800

    1

---
+++ src/settings.ts
@@ -0,0 +1,11 @@
+/**
+ * Creep的默认内存
+ */
+export const creepDefaultMemory: CreepMemory = {
+    role: 'worker',
+    // ready: false,
+    working: false,
+    // spawnRoom: 'W1N1',
+    // pathCache: [],
+    // data:{}
+}
